0bc6b2b5c48dd1996ef5d62d5992fcec4149ff4e
Tim Diercks <tim.r.diercks@gmail.com>
2024-12-06 13:05:30 +0100

    Add solution for part 1

--- day4/part_one.py
+++ day4/part_one.py
@@ -3,37 +3,23 @@ import re
 test = False
 filePath = './test_input.txt' if test else './input.txt'
 
-regex = r'(?=(XMAS|SAMX))'
-
 xmas_occurrences = 0
 
 with open(filePath, 'r') as f:
     data = f.read()
-
-horizontal_xmas = re.findall(regex, data)
-xmas_occurrences += len(horizontal_xmas)
-
-data_matrix = [list(line) for line in data.split('\n')]
+    data_matrix = [list(line) for line in data.split('\n')]
 
 w_matrix = len(data_matrix[0])
 h_matrix = len(data_matrix)
 
-transposed = list(zip(*data_matrix))
-transposed_data = '\n'.join(''.join(row) for row in transposed)
+word_to_find = "XMAS"
 
-vertical_xmas = re.findall(regex, transposed_data)
-xmas_occurrences += len(horizontal_xmas)
-
-def get_next_expected_character(character):
-    if character == "M":
-        return "A"
-    if character == "A":
-        return "S"
-    return None
-
-def find_recursive_xmas(x, y, directions = [(-1,-1), (1,-1), (-1,1), (1,1)], expected_character = "M") -> int:
+def find_recursive_xmas(x, y, directions = [(-1,-1), (1,-1), (-1,1), (1,1), (1,0), (-1,0), (0,1), (0,-1)], current_word = "X") -> int:
     local_occurrences = 0
-
+    if current_word == word_to_find:
+        return 1
+    elif not (current_word in word_to_find):
+        return 0
     for (x_off, y_off) in directions:
         new_x = x + x_off
         new_y = y + y_off
@@ -42,16 +28,8 @@ def find_recursive_xmas(x, y, directions = [(-1,-1), (1,-1), (-1,1), (1,1)], exp
             continue
         if new_y < 0 or new_y == h_matrix:
             continue
-        if data_matrix[new_y][new_x] != expected_character:
-            continue
-        print("")
-        print(y+1,x+1, data_matrix[y][x])
-        print(new_y+1,new_x+1, data_matrix[new_y][new_x])
-        next_expected_character = get_next_expected_character(expected_character)
-        if(next_expected_character == None):
-            print("FOUND")
-            return 1
-        local_occurrences += find_recursive_xmas(new_x, new_y, [(x_off, y_off)], next_expected_character)
+        current_character = data_matrix[new_y][new_x]
+        local_occurrences += find_recursive_xmas(new_x, new_y, [(x_off, y_off)], current_word + current_character)
 
     return local_occurrences
 
@@ -61,9 +39,6 @@ for y in range(len(data_matrix)):
     for x in range(len(data_matrix)):
         if data_matrix[y][x] != "X":
             continue
-        print("_______")
-        print("")
-        print(y+1,x+1, "X")
         
         xmas_occurrences += find_recursive_xmas(x, y)
 
