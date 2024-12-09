b31062b7a950f7b00cbb08e8a79cb5eaaa84409c
Tim Diercks <tim.r.diercks@gmail.com>
2024-12-09 13:07:16 +0100

    Add solution for day 8

--- day8/part_one.py
+++ day8/part_one.py
@@ -1,13 +1,15 @@
-test = True
+test = False
 filePath = './test_input.txt' if test else './input.txt'
 
 with open(filePath, 'r') as f:
     data = f.read()
     data_matrix = [list(line) for line in data.split('\n')]
 
+w = len(data_matrix[0])
+h = len(data_matrix)
 dict = {}
-for y in range(len(data_matrix)):
-    for x in range(len(data_matrix[0])):
+for y in range(h):
+    for x in range(w):
         node = data_matrix[y][x]
         if node in ".":
             continue
@@ -15,5 +17,25 @@ for y in range(len(data_matrix)):
             dict[node] = []
         dict[node].append((x,y))
 
+def is_in_bounds(position: tuple[int,int]):
+    (x,y) = position
+    return x >= 0 and x < w and y >= 0 and y < h
+
+def add_anti_nodes(nodes: tuple[int,int], set: set[tuple[int,int]]):
+    for node in nodes:
+        if is_in_bounds(node):
+            set.add(node)
+
+anti_node_positions = set()
+
 for (node, positions) in dict.items():
-    print(positions)
+    for pos1_index in range(len(positions)-1,-1,-1):
+        (x1, y1) = positions[pos1_index]
+        for pos2_index in range(len(positions)-2,-1,-1):
+            (x2, y2) = positions[pos2_index]
+            (vec_x, vec_y) = (x2-x1, y2-y1)
+            anti_nodes = [(x2+vec_x, y2+vec_y), (x1-vec_x, y1-vec_y)]
+            add_anti_nodes(anti_nodes, anti_node_positions)
+        positions.remove((x1, y1))
+
+print(f"Anti nodes: {len(anti_node_positions)}")
