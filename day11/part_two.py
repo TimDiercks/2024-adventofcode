6a089f5fbe58c323ddcff418285cf509d2faada2
Tim Diercks <tim.r.diercks@gmail.com>
2024-12-11 10:35:14 +0100

    Refactor day 11 part two

--- day11/part_two.py
+++ day11/part_two.py
@@ -14,34 +14,27 @@ with open(filePath, 'r') as f:
             continue
         stones[stone] = 1
 
+def push_or_add(stone, amount, stones):
+    if stone in stones:
+        new_stones[stone] += amount
+    else:
+        new_stones[stone] = amount
+
 while remaining_blinks > 0:
     new_stones = {}
     for stone in stones.keys():
         amount_stones = stones[stone]
         if stone == "0":
-            if "1" in new_stones:
-                new_stones["1"] += amount_stones
-            else:
-                new_stones["1"] = amount_stones
+            push_or_add("1", amount_stones, new_stones)
         elif len(stone) % 2 == 0:
             half_length = len(stone) // 2
             left = stone[:half_length]
             right = re.sub(r"^0+(?!\b)", "", stone[half_length:])
-            if left in new_stones:
-                new_stones[left] += amount_stones
-            else:
-                new_stones[left] = amount_stones
-            if right in new_stones:
-                new_stones[right] += amount_stones
-            else:
-                new_stones[right] = amount_stones
-
+            push_or_add(left, amount_stones, new_stones)
+            push_or_add(right, amount_stones, new_stones)
         else:
             new_gem = str(int(stone)*2024)
-            if new_gem in new_stones:
-                new_stones[new_gem] += amount_stones
-            else:
-                new_stones[new_gem] = amount_stones
+            push_or_add(new_gem, amount_stones, new_stones)
 
     stones = new_stones
     remaining_blinks -= 1
